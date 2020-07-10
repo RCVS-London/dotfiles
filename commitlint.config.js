2d00182978cf33a2aa29490dcd90e758b225ca3f
Nicholas Ferrara <nferrara100@gmail.com>
2020-07-10 11:22:22 +0100

    build(fix): commitlint only warns for line length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "body-max-line-length": [2, "always", 200],
-        "footer-max-line-length": [2, "always", 200],
+        "body-max-line-length": [1, "always", 100],
+        "footer-max-line-length": [1, "always", 100],
         "subject-case": [0],
     },
 };

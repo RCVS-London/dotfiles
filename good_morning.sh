2c7a25ee67af904c69a4882ecfab797b5a5e7ffe
Alex Pilcher <alexpilcher@yahoo.co.uk>
2023-04-13 16:13:28 +0100

    fix: recreate OneCPD virtualenv if Python version has changed

--- good_morning.sh
+++ good_morning.sh
@@ -6,21 +6,39 @@ cd ~/code/OneCPD/
 git pull
 yarn
 
+# Make sure we have the latest pyenv
+brew upgrade
+
 # Get correct python version
 . ~/code/OneCPD/setup/ec2.config
 
 # Check if using the correct python version
-if ! pyenv version | grep -q $PYTHON_VERSION; then
+NEW_PYTHON=false
+if ! pyenv version | grep -q "${PYTHON_VERSION}"; then
+    NEW_PYTHON=true
     # Install it if it is not installed
-    if ! pyenv versions | grep -q $PYTHON_VERSION; then
-        pyenv install $PYTHON_VERSION
+    if ! pyenv versions | grep -q "${PYTHON_VERSION}"; then
+        pyenv install "${PYTHON_VERSION}"
     fi
-    pyenv local $PYTHON_VERSION
+    pyenv local "${PYTHON_VERSION}"
     pip install pre-commit
 fi
 
-source $WORKON_HOME/OneCPD/bin/activate
+if [ "$(command -v deactivate)" = 'deactivate' ]; then
+    deactivate
+fi
 pip install --upgrade pip
+pip install --upgrade virtualenvwrapper
+if [ "${NEW_PYTHON}" = true ]; then
+    PYENV_ROOT="${HOME}"'/.pyenv'
+    source "${PYENV_ROOT}"'/versions/'"${PYTHON_VERSION}"'/bin/virtualenvwrapper.sh'
+    rmvirtualenv OneCPD
+    mkvirtualenv -p "${PYTHON_VERSION}" OneCPD
+fi
+if [ "$(command -v deactivate)" != 'deactivate' ]; then
+    source "${WORKON_HOME}"'/OneCPD/bin/activate'
+fi
+
 poetry self update
 poetry install
 
@@ -28,12 +46,10 @@ cd ~/code/cpd-mobile/
 git pull
 yarn
 
-yarn global add expo-cli
 yarn global upgrade
-brew upgrade
 
 export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+[ -s "${NVM_DIR}"'/nvm.sh' ] && \. "${NVM_DIR}"'/nvm.sh'
 nvm install --lts
 
 echo "Sync complete. Have a great day!"
